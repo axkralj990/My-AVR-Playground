a778cae54a2828d508687470fec805d694812503
axkralj990 <alex.kraljic@gmail.com>
2016-09-05 12:42:20 +0200

    First Commit of all my stuff

---
+++ AK_projects/AK_ADC2Serial_test1/AK_ADC2Serial_test1/Debug/makedep.mk
@@ -0,0 +1,10 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+..\..\AK_libraries\AK_ADC_lib.c
+
+..\..\AK_libraries\USART.c
+
+main.c
+
---
+++ AK_projects/AK_ADC2Serial_test1/AK_ADC2Serial_test1/main.c
@@ -0,0 +1,37 @@
+/*
+ * AK_ADC2Serial_test1.c
+ *
+ * Created: 9/2/2016 3:31:05 PM
+ * Author : Aleksij
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include "AK_ADC_lib.h"
+#include "USART.h"
+#include <stdlib.h>
+
+int main(void)
+{
+	uint8_t sensorValue;
+	char str[16];
+	
+    initUSART();
+	init_ADC(5,8,8);
+	
+	//start_FreeRunning_ADC();
+	
+	DDRC |= (1<<DDC4);
+	PORTC |= (1<<PORTC4);
+	
+    while (1) 
+    {
+		start_SingleConversion_ADC();
+		sensorValue = read_ADC_8bit();
+		itoa(sensorValue,str,10);
+		printString(str);
+		transmitByte(10);
+		_delay_ms(20);
+    }
+}
+
